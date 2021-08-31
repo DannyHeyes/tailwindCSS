cf245636095b56556b927521f4a1d2df1c48cabd
Danny Heyes <danny.heyes01@gmail.com>
2021-08-31 16:11:46 +0200

    img update

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ return (
 
           {destinations.map(destination => (
             <div key={destination.city} className="flex items-center rounded-lg bg-white shadow-lg overflow-hidden m-4">
-                    <img className="h-32 w-32 flex-shrink-0" src={destination.imageUrl} alt={destination.imageAlt}/>
+                    <img className="h-32 w-32 flex-shrink-0 flex-grow" src={destination.imageUrl} alt={destination.imageAlt}/>
                     <div className="px-6 py4">
                         <h3 className="text-lg font-semi-bold text-gray-800">{destination.city}</h3>
                         <p className="text-gray-600">${destination.averagePrice} / night average</p>
